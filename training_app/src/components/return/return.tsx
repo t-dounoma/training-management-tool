93bd0a6a096bfe7c08e60213d7ecdc16f46ff179
t-dounoma <t.dounomae.sys21@morijyobi.ac.jp>
2023-11-16 15:02:30 +0900

    [feat,style] returnボタンの作成
    - Topページへ遷移するためのボタンを作成しました。
    - cssの適用を行いました。
    - UIの言語を日本語に修正しました。

--- training_app/src/components/return/return.tsx
+++ training_app/src/components/return/return.tsx
@@ -1,18 +1,17 @@
-// src/components/return.tsx
 import React from 'react';
 import { useRouter } from 'next/router';
+import styles from './return.module.css';
 
 const ReturnButton: React.FC = () => {
   const router = useRouter();
 
   const handleReturn = () => {
-    // Navigate to the user's top page
     router.push('/dashboard/[userId]', `/dashboard/${sessionStorage.getItem('userId')}`);
   };
 
   return (
-    <button onClick={handleReturn}>
-      Return
+    <button className={styles.button} onClick={handleReturn}>
+      Topに戻る
     </button>
   );
 };
